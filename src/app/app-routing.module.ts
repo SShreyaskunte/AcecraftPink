f623dea9421451e898b4047b3c999153da4029ae
Sachinkrishna2000 <krishsachin2000@gmail.com>
2022-10-11 11:22:51 +0530

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+
 import { CollegeComponent } from './college/college.component';
 import { HomeComponent } from './home/home.component';
 import { EnterpriseComponent } from './enterprise/enterprise.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { OrderstatusComponent } from './orderstatus/orderstatus.component';
+
+
+
+
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'college',component:CollegeComponent},
-  {path: 'enterprise', component:EnterpriseComponent}
-=======
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
-
-const routes: Routes = [
+  {path: 'enterprise', component:EnterpriseComponent},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
-  {path:'forgotpass',component:ForgotpasswordComponent}
+  {path:'forgotpass',component:ForgotpasswordComponent},
+  {path:'orderstatus',component:OrderstatusComponent},
 
->>>>>>> 6b9074139413c5d695f62da1aa66a8a35b7d5ab9
+
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,33 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
+
 import { NavComponent } from './nav/nav.component';
-import { BodyComponent } from './body/body.component';
+
 import { CollegeComponent } from './college/college.component';
 import { EnterpriseComponent } from './enterprise/enterprise.component';
-=======
 
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 import { OrderstatusComponent } from './orderstatus/orderstatus.component';
 
->>>>>>> 6b9074139413c5d695f62da1aa66a8a35b7d5ab9
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-<<<<<<< HEAD
+
     NavComponent,
-    BodyComponent,
     CollegeComponent,
-    EnterpriseComponent
-=======
+    EnterpriseComponent,
     SignupComponent,
     LoginComponent,
     ForgotpasswordComponent,
     OrderstatusComponent
     
->>>>>>> 6b9074139413c5d695f62da1aa66a8a35b7d5ab9
+
   ],
   imports: [
     BrowserModule,
