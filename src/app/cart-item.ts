8aa86aa2411eab3dc067a912b3521865f6d38581
Sachinkrishna2000 <krishsachin2000@gmail.com>
2022-10-14 15:08:16 +0530

    Commit

---
+++ src/app/cart-item.ts
@@ -0,0 +1,11 @@
+export class CartItem {
+    id:number|any;
+    pname:string|undefined;
+    size:number|any;
+    // length:number|any;
+    // waist:number|any;
+    price:number|any;
+    pimage:string|undefined;
+    quantity:number|any;
+    totalPrice:number|any;
+}
